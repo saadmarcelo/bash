f7206f704d65020fbcf98d640da6f2142e007740
Marcelo saad <marcelo_saad@hotmail.com>
2024-03-22 19:54:58 -0400

    gerar backup

--- listaExercicioCondicional/GeraBackupHome.sh
+++ listaExercicioCondicional/GeraBackupHome.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-if [ -d $HOME/Backup ]
+FOLDER=$HOME/Backup
+
+if [ ! -d $FOLDER ]
 then 
-  echo "Diretorio Backup existe"
-else
-  mkdir $HOME/backup
-  echo "Criando diretorio Backup"
+  echo "Criando Diretorio $FOLDER"
+  mkdir -p $FOLDER
 fi
 
-NAME=$(date +'%Y%m%d%H%M')
-echo "$NAME"
+DAYS7=$(find $FOLDER -ctime -7 -name backup_home\*tgz)
 
-find $HOME/backup/ -type f -ctime +7 
-  if [ $? -eq 0 ]
+if [ "$DAYS7" ]  # Testa se a variavel é nula
+then
+  echo "Já foi gerado um backup do diretorio $FOLDER nos ultimos 7 dias."
+  echo -n "Deseja continuar? (N/s): "
+  read -n1 CONT
+  echo
+  if [ "$CONT" = N -o "$CONT" = n -o "$CONT" = "" ]
   then
-    echo ""
+    echo "Backup Abordado!"
+    exit 1
+  elif [ $CONT = S -o $CONT = s ]
+  then
+    echo "Será criado mais um backup para a mesma semana"
+  else
+      echo "Opção Inválida"
+      exit 2
+  fi
+fi
+
+echo "Criando Backup"
+ARQ="backup_home_$(date +%Y%m%d%H%M).tgz"
+tar zcvpf $FOLDER/$ARQ --absolute-names --exclude="$FOLDER" "$HOME"/* > /dev/null
+
+echo
+echo "O backup de nome \""$ARQ"\" foi criado em $FOLDER"
+echo
+echo "Backup Concluido!"
+
